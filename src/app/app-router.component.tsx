561492189073f07921ec1b590b4d5463fc6962b6
Shubham Bakhal <68225437+shubham-bakhal@users.noreply.github.com>
2023-05-13 19:43:41 +0530

    uncommented routes for auth (#8)

--- src/app/app-router.component.tsx
+++ src/app/app-router.component.tsx
@@ -2,21 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./app.component";
 import Logout from "./auth/logout.component";
 
+import Auth from "./auth/auth.component";
+import Login from "./auth/login.component";
 import { Editor } from "./editor";
+import PrivateRoute from "./shared/components/private-route.component";
 
 function AppRouter() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route element={<App />}>
-					{/* <Route element={<Auth />}> */}
-					{/* <Route index element={<Login />} /> */}
-					{/*  make login permission AKA protected routes */}
-					{/* <Route element={<PrivateRoute />}> */}
-					<Route path="/editor/:sessionId" element={<Editor />} />
-					<Route path="/logout" element={<Logout />} />
-					{/* </Route> */}
-					{/* </Route> */}
+					<Route element={<Auth />}>
+						<Route index element={<Login />} />
+						{/* make login permission AKA protected routes */}
+						<Route element={<PrivateRoute />}>
+							<Route
+								path="/editor/:sessionId"
+								element={<Editor />}
+							/>
+							<Route path="/logout" element={<Logout />} />
+						</Route>
+					</Route>
 
 					<Route path="*" element={<h1>404</h1>} />
 				</Route>
